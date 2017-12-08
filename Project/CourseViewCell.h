d65aca30722154d9cb9892e2e1c2250aa555aeaa
mac <mac@pc-205-69.customer.leeds.opal.lan>
2017-12-08 11:46:45 +0000

    set up cell to show timetable

---
+++ Project/CourseViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CourseViewCell.h
+//  Project
+//
+//  Created by mac on 2017/12/7.
+//  Copyright © 2017年 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CourseViewCell : UITableViewCell
+
+@end
---
+++ Project/CourseViewCell.m
@@ -0,0 +1,24 @@
+//
+//  CourseViewCell.m
+//  Project
+//
+//  Created by mac on 2017/12/7.
+//  Copyright © 2017年 University of Leeds. All rights reserved.
+//
+
+#import "CourseViewCell.h"
+
+@implementation CourseViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+    
+    // Configure the view for the selected state
+}
+
+@end
