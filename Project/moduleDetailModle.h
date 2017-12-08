bbaf793699f0e9d9ef4b486ecfcce7399c9eb84f
mac <mac@pc-205-69.customer.leeds.opal.lan>
2017-12-08 10:46:03 +0000

    create datamodel for the major

---
+++ Project/moduleDetailModle.h
@@ -0,0 +1,18 @@
+//
+//  moduleDetailModle.h
+//  Project
+//
+//  Created by mac on 2017/12/8.
+//  Copyright © 2017年 University of Leeds. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface moduleDetailModle : NSObject
+
+@property (nonatomic, copy) NSString *modulecode;
+@property (nonatomic, copy) NSString *credit;
+@property (nonatomic, copy) NSString *staff;
+@property (nonatomic, copy) NSString *modulename;
+
+@end
---
+++ Project/moduleDetailModle.m
@@ -0,0 +1,13 @@
+//
+//  moduleDetailModle.m
+//  Project
+//
+//  Created by mac on 2017/12/8.
+//  Copyright © 2017年 University of Leeds. All rights reserved.
+//
+
+#import "moduleDetailModle.h"
+
+@implementation moduleDetailModle
+
+@end
