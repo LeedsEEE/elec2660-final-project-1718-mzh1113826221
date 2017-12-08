fbbd8ecbbfbc67e05c3815f068547a9364ae993d
mac <mac@pc-205-69.customer.leeds.opal.lan>
2017-12-08 13:36:26 +0000

    Used to show data for the timetable.the collection view is to display the screen.I find the courseviewcell is not good enough.So i definite the courescollectionviewlayout

---
+++ Timetable/CouresCollectionViewLayout.h
@@ -0,0 +1,15 @@
+//
+//  CouresCollectionViewLayout.h
+//  Project
+//
+//  Created by mac on 2017/11/30.
+//  Copyright © 2017年 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CouresCollectionViewLayout : UICollectionViewLayout
+@property(nonatomic,assign)CGFloat width;
+@property(nonatomic,assign)CGFloat height;
+@property(nonatomic,strong)NSMutableArray*array;
+@end
