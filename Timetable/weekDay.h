737951deea4ab6345b80cd65f62de85af4202f99
mac <mac@macdemacbook-pro.local>
2017-12-08 13:26:39 +0000

    data for week, used for timetable

---
+++ Timetable/weekDay.h
@@ -0,0 +1,13 @@
+//
+//  weekDay.h
+//  Project
+//
+//  Created by mac on 2017/11/30.
+//  Copyright © 2017年 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface weekDay : UIView
+-(void)setDay:(NSString*)Day week:(NSString*)Week;
+@end
